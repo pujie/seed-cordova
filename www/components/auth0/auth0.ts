268ba55b86efd3b35a0ea830f1221f81fbb9812e
Louis Larry <louis.larry@gmail.com>
2015-11-11 14:28:50 +0700

    wip auth0

--- www/components/auth0/auth0.ts
+++ www/components/auth0/auth0.ts
@@ -1,10 +1,35 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
-export default angular.module('app.auth0', [])
-.controller('Auth0Controller', Auth0Controller)
+export default angular.module('app.auth0', [
+    'auth0',
+    require('angular-storage'),
+    require('angular-jwt'),
+])
+    .config(function(authProvider) {
+	authProvider.init({
+	    domain: 'louislarry.auth0.com',
+	    clientID: 'yrZ74GnnkjNpjKDbWlLPVtMkyTetcwm7',
+	});
+    })
+    .run(function(auth) {
+	auth.hookEvents();
+    })
+    .controller('Auth0Controller', Auth0Controller);
 
-Auth0Controller.$inject = ['$scope'];
+function Auth0Controller($http, auth, store, $location) {
+    this.login = () => {
+	auth.signin({}, (profile, token) => {
+	    store.set('profile', profile);
+	    store.set('token', token);
+	    $location.path('/');
+	}, function() {
+	    // Error callback
+	});
+    }
 
-function Auth0Controller($scope) {
-this.myname = 'auth0';
+    this.logout = () => {
+
+    }
 }
+
+Auth0Controller.$inject = ['$http', 'auth', 'store', '$location'];
