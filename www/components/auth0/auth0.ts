bb7d6f9230d656752976369eb066f14990a1b632
Louis Larry <louis.larry@gmail.com>
2015-11-12 09:58:31 +0700

    add auth0 login sample page

--- www/components/auth0/auth0.ts
+++ www/components/auth0/auth0.ts
@@ -22,26 +22,77 @@ Auth0Config.$inject = ['authProvider'];
 function Auth0Config(authProvider) {
     authProvider.init({
   	domain: 'louislarry.auth0.com',
+  	// clientId: 'yrZ74GnnkjNpjKDbWlLPVtMkyTetcwm7',
   	clientID: 'yrZ74GnnkjNpjKDbWlLPVtMkyTetcwm7',
         callbackUrl: location.href,
-        loginState: 'login'
+        loginState: 'auth0',
+	// sso: true,
     });
+
+    authProvider.on('authenticated', ['$location', function($location) {
+	console.log('on authenticated');
+    }]);
+
+    authProvider.on('loginSuccess',
+		    ['profilePromise', 'idToken', '$location', 'store',
+		     function(profilePromise, idToken, $location, store) {
+			 console.log('on loginSuccess');
+			 profilePromise.then( profile => {
+			     store.set('profile', profile);
+			     store.set('token', idToken);
+
+			 });
+			 $location.path('/');
+		     }]);
+
+    authProvider.on('loginFailure', ['error', '$location', function (error, $location) {
+	console.log('loginFailure');
+	$location.path('/error');
+    }]);
+
+    authProvider.on('logout', () => {
+	console.log('on logout');
+    });
+
+    authProvider.on('forbidden', () => {
+	console.log('on forbidden');
+    });
+}
+
+successCb.$inject = ['profilePromise', 'idToken', '$location', 'store'];
+function successCb (profilePromise, idToken, $location, store) {
+    console.log('successCb');
+}
+
+errorCb.$inject = ['err', '$location'];
+function errorCb (err, $location) {
+    console.log('errorCb');
 }
 
 Auth0Controller.$inject = ['$http', 'auth', 'store', '$location'];
 
 function Auth0Controller($http, auth, store, $location) {
+    this.profile = auth.profile;
+    this.isAuthenticated = auth.isAuthenticated;
+
     this.login = () => {
-	auth.signin({}, (profile, token) => {
-	    store.set('profile', profile);
-	    store.set('token', token);
-	    $location.path('/');
-	}, function() {
-	    // Error callback
-	});
+	auth.signin({ popup: true }, successCb, errorCb);
+    }
+
+    this.signup = () => {
+	console.log('Auth0Controller signup');
+	auth.signup({ popup: true }, successCb, errorCb);
+    }
+
+    this.reset = () => {
+	console.log('Auth0Controller logout');
+	auth.reset({ popup: true }, successCb, errorCb);
     }
 
     this.logout = () => {
-
+	console.log('Auth0Controller logout');
+	auth.signout();
     }
+
+    this.login();
 }
